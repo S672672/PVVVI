86259517219b62dc65a5fd78278b43e0064a839f
S672672 <bsmith672@rku.ac.in>
2024-01-31 11:34:10 +0530

    scrollable made

--- src/Components/CardsSection.jsx
+++ src/Components/CardsSection.jsx
@@ -3,7 +3,7 @@ import Cards from './Cards'
 
 function CardsSection() {
   return (
-    <div className='m-10 h-96 w-screen flex justify-center items-center gap-4 '> 
+    <div className='m-10 h-96 w-screen flex justify-center items-center gap-4 overflow-x-auto flex-no-wrap'> 
         <Cards picture="https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iRlXCwCHJ45U/v2/-1x-1.jpg" isAvailable="Available ->"/>
         <Cards picture="https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iRlXCwCHJ45U/v2/-1x-1.jpg" isAvailable="Available ->"/>
         <Cards picture="https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iRlXCwCHJ45U/v2/-1x-1.jpg" isAvailable="Available ->"/>
