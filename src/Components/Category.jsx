3f7e80d7865793f3ebcb78e3147f64c81f48a013
S672672 <bsmith672@rku.ac.in>
2024-02-04 22:18:39 +0530

    homepage completed

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -3,10 +3,13 @@ import InsideCategory from './InsideCategory'
 
 export default function Category() {
   return (
-    <div className='h-40 w-full flex items-center justify-center m-10 gap-5'>
-    <InsideCategory image="" type="dog" />
-    <InsideCategory image="" type="cat" />
-    <InsideCategory image="" type="sheep" />
+    <div className='h-25 w-full flex items-center justify-evenly m-10 gap-5'>
+    <InsideCategory image="./src/assets/dog1.jpg" type="dog" />
+    <InsideCategory image="./src/assets/cat.jpeg" type="cat" />
+    <InsideCategory image="./src/assets/rabbit.jpg" type="rabbit" />
+    <InsideCategory image="./src/assets/rabbit.jpg" type="rabbit" />
+    <InsideCategory image="./src/assets/rabbit.jpg" type="rabbit" />
+    <InsideCategory image="./src/assets/rabbit.jpg" type="rabbit" />
     <InsideCategory image="" type="lion" />
     <InsideCategory image="" type="Tiger" />
     </div>
--- src/Components/InsideCategory.jsx
+++ src/Components/InsideCategory.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function InsideCategory({image,type}) {
   return (
-    <div className='h-30 flex flex-col justify-center items-center'>
-         <img src={image} alt='dog'></img>
-    <div className=''>{type}</div>
+    <div className='h-15 flex flex-col justify-evenly items-center  object-cover rounded-t-lg transform hover:scale-105 transition-transform duration-300 cursor-pointer'>
+         <img src={image} className='h-10 rounded-3xl' alt='dog'></img>
+    <div className='font-bold text-xl'>{type}</div>
     </div>
   )
 }
