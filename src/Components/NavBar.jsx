2585ed44337a1232d8c61d188535c8e784130f52
S672672 <bsmith672@rku.ac.in>
2024-02-08 22:50:58 +0530

    landing page fixed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 
-export default function Navbar(){
+export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-white p-6 border-4 border-b">
+    <nav className="flex items-center justify-between bg-white p-6 border-4 border-b">
       <div className="flex items-center flex-shrink-0 text-black mr-6">
         <span className="font-semibold text-xl tracking-tight">PetAdopt</span>
       </div>
       <div className="block lg:hidden">
         <button className="flex items-center px-3 py-2 border rounded text-gray-800 border-gray-800 hover:text-black hover:border-black">
-          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+          </svg>
         </button>
       </div>
-      <div className="w-full flex flex-grow lg:flex lg:items-end lg:w-auto justify-end">
-        <div className="text-sm lg:flex-grow text-xl font-bold lg:pl-96 lg:ml-96">
-          <a href="#search" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
-            Adopt
-          </a>
-          <a href="#adopt" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
-            Give
-          </a>
-          <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
-            Happening
-          </a>
-          <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
-            Playground
-          </a>
-        </div>
+      <div className="w-full flex items-center justify-end lg:justify-start lg:w-auto gap-10">
+      <div className="text-sm lg:flex-grow text-xl font-bold flex items-center justify-between">
+  <a href="#search" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-2 lg:mr-4">
+    Adopt
+  </a>
+  <a href="#adopt" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-2 lg:mr-4">
+    Give
+  </a>
+  <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-2 lg:mr-4">
+    Happening
+  </a>
+  <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black">
+    Playground
+  </a>
+</div>
+
         <div>
-        <button className="block mt-4 lg:inline-block lg:mt-0 text-white bg-green-500 hover:bg-green-700 py-2 px-4 rounded-lg mr-4">
+          <button className="block mt-4 lg:inline-block lg:mt-0 text-white bg-green-500 hover:bg-green-700 py-2 px-4 rounded-lg mr-4">
             Log In
           </button>
           <button className="block mt-4 lg:inline-block lg:mt-0 text-white bg-orange-500 hover:bg-orange-700 py-2 px-4 rounded-lg">
@@ -37,4 +41,4 @@ export default function Navbar(){
       </div>
     </nav>
   );
-};
+}
--- src/Components/Process.jsx
+++ src/Components/Process.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Process() {
   return (
-    <div className='flex flex-col items-center justify-center m-10 gap-3'>
-        <p className='text-black font-bold text-3xl'>Ready to adopt or give?</p>
+    <div className='flex flex-col items-center justify-center m-10 gap-5'>
+        <p className='text-black font-bold text-5xl flex flex-col items-center justify-center'>Ready to adopt or <span> give? </span></p>
         <button className="bg-green-400 hover:bg-yellow-400 text-black font-bold py-3 px-6 rounded-full text-lg">Find a pet</button>
         <button className="bg-gray-300 hover:bg-blue-400 text-black font-bold py-3 px-6 rounded-full text-lg">Give a pet</button>
     </div>
