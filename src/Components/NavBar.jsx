b5a45e7fcf4c1b140476dfd6e5e8fedc5929d5f6
S672672 <bsmith672@rku.ac.in>
2024-02-08 16:00:26 +0530

    small change

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,42 +1,40 @@
 import React from 'react';
 
-const Navbar = () => {
+export default function Navbar(){
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6 md-5">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className="flex items-center justify-between flex-wrap bg-white p-6 border-4 border-b">
+      <div className="flex items-center flex-shrink-0 text-black mr-6">
         <span className="font-semibold text-xl tracking-tight">PetAdopt</span>
       </div>
       <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <button className="flex items-center px-3 py-2 border rounded text-gray-800 border-gray-800 hover:text-black hover:border-black">
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div>
-      <div className="w-full block flex flex-grow lg:flex lg:items-center lg:w-auto justify-between">
-        <div className="text-sm lg:flex-grow">
-          <a href="#search" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      <div className="w-full block flex flex-grow lg:flex lg:items-center lg:w-auto justify-between lg:ml-96">
+        <div className="text-sm lg:flex-grow text-xl font-bold">
+          <a href="#search" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
             Adopt
           </a>
-          <a href="#adopt" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <a href="#adopt" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
             Give
           </a>
-          <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+          <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
             Happening
           </a>
-          <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+          <a href="#give" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-black mr-4">
             Playground
           </a>
         </div>
         <div>
-          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        <button className="block mt-4 lg:inline-block lg:mt-0 text-white bg-green-500 hover:bg-green-700 py-2 px-4 rounded-lg mr-4">
             Log In
-          </a>
-          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white py-2 px-4 rounded">
+          </button>
+          <button className="block mt-4 lg:inline-block lg:mt-0 text-white bg-orange-500 hover:bg-orange-700 py-2 px-4 rounded-lg">
             Sign Up
-          </a>
+          </button>
         </div>
       </div>
     </nav>
   );
 };
-
-export default Navbar;
