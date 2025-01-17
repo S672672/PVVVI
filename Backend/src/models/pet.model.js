81f813c2cbe5309b30b7d5ad5b2799cd3c90b0cd
s672672 <bsmith672@rku.ac.in>
2025-01-17 13:12:26 +0545

    a little change

--- Backend/src/models/pet.model.js
+++ Backend/src/models/pet.model.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 const petSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
+    species:{type:String,required:true},
     breed: { type: String, required: true },
+    size:{type:String,enum:["small","medium","large"]
+    },
+    spayed:{type:String,required:true},
+    vaccinated:{type:String,required:true},
     description: { type: String },
     image: { type: String }, 
-    isApproved: { type: Boolean, default: false },
+    status: {
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: 'pending',
+  },
+
     submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   }, { timestamps: true });
   
